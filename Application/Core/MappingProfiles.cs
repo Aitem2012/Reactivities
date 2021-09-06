34c2f69f22c9b53930a1c58d33f9761f5932546a
Aitem2012 <ibrahim.ajumoko@decagon.dev>
2021-09-06 07:06:18 +0100

    Entity relationship

--- Application/Core/MappingProfiles.cs
+++ Application/Core/MappingProfiles.cs
@@ -1,3 +1,5 @@
+using System.Linq;
+using Application.Activities;
 using AutoMapper;
 using Domain;
 
@@ -8,6 +10,14 @@ namespace Application.Core
         public MappingProfiles()
         {
             CreateMap<Activity, Activity>();
+            CreateMap<Activity, ActivityDto>()
+                .ForMember(x => x.HostUsername, p => p.MapFrom(x => x.Attendees
+                .FirstOrDefault(b => b.IsHost).AppUser.UserName));
+
+            CreateMap<ActivityAttendee, Profiles.Profile>()
+                .ForMember(x => x.DisplayName, o => o.MapFrom(s => s.AppUser.DisplayName))
+                .ForMember(x => x.Username, o => o.MapFrom(s => s.AppUser.UserName))
+                .ForMember(x => x.Bio, o => o.MapFrom(s => s.AppUser.Bio));
         }
     }
 }
