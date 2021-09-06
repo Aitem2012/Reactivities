14e5388dc99a643436c3985bac880a56d5e07322
Aitem2012 <ibrahim.ajumoko@decagon.dev>
2021-09-06 07:06:18 +0100

    Entity relationship

--- Application/Activities/Create.cs
+++ Application/Activities/Create.cs
@@ -1,9 +1,11 @@
 using System.Threading;
 using System.Threading.Tasks;
 using Application.Core;
+using Application.Interfaces;
 using Domain;
 using FluentValidation;
 using MediatR;
+using Microsoft.EntityFrameworkCore;
 using Persistence;
 
 namespace Application.Activities
@@ -12,8 +14,8 @@ namespace Application.Activities
     {
         public class Command : IRequest<Result<Unit>>
         {
-            public Activity Activity {get; set;}
-            
+            public Activity Activity { get; set; }
+
         }
 
         public class CommandValidator : AbstractValidator<Command>
@@ -27,18 +29,29 @@ namespace Application.Activities
         public class Handler : IRequestHandler<Command, Result<Unit>>
         {
             private readonly DataContext _context;
+            private readonly IUserAccessor _userAcccessor;
 
-            public Handler(DataContext context)
+            public Handler(DataContext context, IUserAccessor userAcccessor)
             {
+                _userAcccessor = userAcccessor;
                 _context = context;
             }
 
             public async Task<Result<Unit>> Handle(Command request, CancellationToken cancellationToken)
             {
+                var user = await _context.Users.FirstOrDefaultAsync(x => x.UserName == _userAcccessor.GetUsername());
+
+                var attendee = new ActivityAttendee{
+                    AppUser = user,
+                    Activity = request.Activity,
+                    IsHost = true
+                };
+                request.Activity.Attendees.Add(attendee);
                 _context.Activities.Add(request.Activity);
 
-                var result = await _context.SaveChangesAsync()> 0;
-                if(!result) return Result<Unit>.Failure("Failed to create Activity");
+
+                var result = await _context.SaveChangesAsync() > 0;
+                if (!result) return Result<Unit>.Failure("Failed to create Activity");
 
                 return Result<Unit>.Success(Unit.Value);
             }
